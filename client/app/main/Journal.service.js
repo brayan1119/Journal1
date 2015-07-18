208f302e325c3fb56c70f992304e7adec2b43c8d
brayan <brayan1119@gmail.com>
2015-07-18 16:26:50 -0500

    busqueda commpleta

--- client/app/main/Journal.service.js
+++ client/app/main/Journal.service.js
@@ -3,8 +3,11 @@
 angular.module('journalApp')
   .service('Journal', function ($http) {
     var self=this;
-    self.getJournal = function(callback){
-      $http.get('http://localhost:8080/Journal/Controller/prueba')
+    self.getJournal = function(callback,palabra){
+      if( palabra == undefined ){
+        palabra = '';
+      };
+      $http.get('http://localhost:8080/Journal/Controller/prueba1?Buscar='+palabra)
         .success(function(data){
           callback(data);
         })
--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -4,13 +4,18 @@ angular.module('journalApp')
   .controller('MainCtrl', function ($scope,Journal) {
 
     var init = function (){
-      Journal.getJournal(callbackJournal);
+      Journal.getJournal(callbackJournal,$scope.palabra);
     };
     var callbackJournal = function(data){
       $scope.journal =data;
 
-      $scope.pageSize=1000;
+      $scope.pageSize=500;
       $scope.currentPage=1;
+
+    };
+
+    $scope.change = function(){
+      init();
     };
 
     init();
